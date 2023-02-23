3974492fc3491b9d06a8f92b1b171108319e6da0
Talha Waheed <talhawaheed57@gmail.com>
2023-02-23 12:58:17 -0600

    Modified stuff

--- envoy-img/docker-entrypoint.sh
+++ envoy-img/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Generating envoy.yaml config file..."
-cat /tmpl/envoy.yaml.tmpl | envsubst \$SERVICE_NAME,\$ENVOY_LB_ALG > /etc/envoy.yaml
+cat /tmpl/envoy.yaml.tmpl | envsubst \$SERVICE_NAME,\$ENVOY_LB_ALG,\$TIMEOUT > /etc/envoy.yaml
 
 echo "Starting Envoy..."
 /usr/local/bin/envoy -c /etc/envoy.yaml
